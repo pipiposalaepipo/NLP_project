3991e97cc3f9858a0f8c65dbfdae7e4f29636c98
pipiposalaepipo <116275928+pipiposalaepipo@users.noreply.github.com>
2026-03-24 23:20:35 +0700

    fix web site

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,20 @@
 from flask import Flask
+import os
+
 def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = ' Eieiza'
-    from view import views
+    # ใช้ os.path.dirname(os.path.abspath(__file__)) เพื่อหาตำแหน่งที่แท้จริงของโฟลเดอร์ website
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    template_dir = os.path.join(base_dir, 'templates')
+    
+    app = Flask(__name__, template_folder=template_dir)
+    
+    app.config['SECRET_KEY'] = 'Eieiza'
+    app.config['UPLOAD_FOLDER'] = os.path.join(base_dir, 'uploads')
+    
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+
+    from .view import views 
     app.register_blueprint(views, url_prefix='/')
+
     return app
